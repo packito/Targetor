a3bf001d123b09b9db03e414e211e11755cff851
packito <packito1@gmail.com>
2013-05-27 13:50:32 +0200

    First project commit!

---
+++ Targetor!/src/cz/packito/targetor/MenuActivity.java
@@ -0,0 +1,16 @@
+package cz.packito.targetor;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+
+	}
+
+}
